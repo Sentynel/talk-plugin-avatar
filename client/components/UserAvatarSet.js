59618cb8a3f8dfccfd41c00a092f21562746c9bd
Sam Lade <sam@sentynel.com>
2019-05-15 18:15:51 +0000

    feedback on successful avatar set

--- client/components/UserAvatarSet.js
+++ client/components/UserAvatarSet.js
@@ -27,27 +27,30 @@ const withSetAvatar = withMutation(
 class UserAvatarSet extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {value: ''};
+        this.state = {value: '', button: 'Set avatar'};
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleChange(event) {
-        this.setState({value: event.target.value});
+        this.setState({value: event.target.value, button: 'Set avatar'});
     }
 
     handleSubmit(event) {
         event.preventDefault();
-        this.props.setAvatar(this.state.value);
+        this.props.setAvatar(this.state.value)
+        .then(result => {this.setState({button: 'Set avatar ✓'});})
+        .catch(result => {this.setState({button: 'Set avatar x'});});
     }
 
     render() {
         return <section className={'talk-plugin-avatar-section'}>
             <h3>Avatar</h3>
+            <p>Set an avatar image here. It will need to be hosted elsewhere - we recommend <a href="https://imgur.com">Imgur</a>. Upload the image, then once it's done uploading, right click it and select "Copy image address", and paste that into the box. Please try and keep avatars small - you can use Imgur's resize tool (hover over the image, click the dropdown, click "Edit image", and resize to 100x100 or so, then "Apply" and "Save").</p>
             <form onSubmit={this.handleSubmit}>
             <TextField type="url" label="Avatar URL" value={this.state.value} onChange={this.handleChange} />
-            <Button type="submit">Set avatar</Button>
+            <Button type="submit">{this.state.button}</Button>
             </form>
             </section>;
     }
