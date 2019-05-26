b98efde8349084b9035052acbc3f1992c7a5d9a8
Sam Lade <sam@sentynel.com>
2019-05-26 12:15:01 +0000

    add some validation for image urls

--- client/components/UserAvatarSet.js
+++ client/components/UserAvatarSet.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, TextField } from 'plugin-api/beta/client/components/ui';
+import { Alert, Button, TextField } from 'plugin-api/beta/client/components/ui';
 import { withMutation, withFragments } from 'plugin-api/beta/client/hocs';
 import { gql } from 'react-apollo';
 
@@ -36,7 +36,7 @@ const withGetAvatar = withFragments({
 class UserAvatarSet extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {value: '', button: 'Set avatar'};
+        this.state = {value: '', button: 'Set avatar', error: ''};
         if (props.root.me.avatar) {
             this.state.value = props.root.me.avatar;
         }
@@ -46,7 +46,14 @@ class UserAvatarSet extends React.Component {
     }
 
     handleChange(event) {
-        this.setState({value: event.target.value, button: 'Set avatar'});
+        var url = event.target.value;
+        var error = '';
+        if (!/\.(?:png|jpg|jpeg|gif)$/.test(url)) {
+            error = 'Your URL should end with .png or .jpg - try right clicking the image you want and \'Copy image address\'.';
+        } /*else if (/^http:\/\//.test(url)) {
+            error = 'http:// images may not work in all browsers - use a host which supports https://';
+        }*/
+        this.setState({value: url, button: 'Set avatar', error: error});
     }
 
     handleSubmit(event) {
@@ -62,6 +69,7 @@ class UserAvatarSet extends React.Component {
             <p>Set an avatar image here. It will need to be hosted elsewhere - we recommend <a href="https://imgur.com" target="_blank">Imgur</a>. Upload the image, then once it's done uploading, right click it and select "Copy image address", and paste that into the box. Please try and keep avatars small - you can use Imgur's resize tool (hover over the image, click the dropdown, click "Edit image", and resize to 100x100 or so, then "Apply" and "Save").</p>
             <form onSubmit={this.handleSubmit}>
             <TextField type="url" label="Avatar URL" value={this.state.value} onChange={this.handleChange} />
+            { this.state.error && <Alert>{this.state.error}</Alert> }
             <Button type="submit">{this.state.button}</Button>
             </form>
             </section>;
